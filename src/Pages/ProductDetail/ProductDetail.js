776d69d464479e64cc9f5cc9680d3503218fa1c6
Sewyishal Netsanet <sewyishalx@gmail.com>
2025-09-25 11:14:54 +0300

    Added detailPage routing

--- src/Pages/ProductDetail/ProductDetail.js
+++ src/Pages/ProductDetail/ProductDetail.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classes from "./ProductDetail.module.css";
 import LayOut from "../../components/LayOut/LayOut";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import ProductCard from "../../components/Product/ProductCard";
+import { producturl } from "../../Api/endPoint";
+
 const ProductDetail = () => {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`${producturl}/products/${id}`)
+      .then((res) => {
+        setProduct(res.data);
+        console.log("Fetched product:", res.data);
+      })
+      .catch((err) => {
+        console.error("Error fetching product:", err);
+      });
+  }, [id]);
+
+  if (!product) {
+    return (
+      <LayOut>
+        <p>Loading product details...</p>
+      </LayOut>
+    );
+  }
+
   return (
     <LayOut>
-      <div>ProductDetail</div>
+      <ProductCard product={product} key={product.id} />
     </LayOut>
   );
 };
--- src/components/Product/ProductCard.js
+++ src/components/Product/ProductCard.js
@@ -2,20 +2,18 @@ import React from "react";
 import Rating from "@mui/material/Rating";
 import CurrencyFormat from "../CurrencyFormat/CurrencyFormat";
 import classes from "./Product.module.css";
-
+import { Link } from "react-router-dom";
 const ProductCard = ({ product }) => {
-  // ✅ lowercase 'product'
-  const { title, image, price, rating } = product;
+  const { title, image, id, price, rating } = product;
 
   return (
     <div className={classes.card__container}>
-      <a href="/">
-        <img src={image} alt={title} /> {/* ✅ better alt */}
-      </a>
+      <Link to={`/product/${id}`}>
+        <img src={image} alt={title} />
+      </Link>
       <div>
         <h3>{title}</h3>
         <div className={classes.rating}>
-          {/* ✅ use rating.rate instead of whole object */}
           <Rating value={rating?.rate} precision={0.1} readOnly />
           <small>{rating?.count} reviews</small>
         </div>
